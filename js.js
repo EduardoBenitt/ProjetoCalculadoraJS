9cd2f0d93f8662bbb65f9d8ef69378b85db16a00
EduardoBenitt <benitt.eduardo@gmail.com>
2023-09-03 01:03:55 -0400

    Adicionado resoluções de problemas lógicos, para evitar erros nos calculos.

--- js.js
+++ js.js
@@ -1,29 +1,74 @@
-
-
 const botoes = document.querySelectorAll('button');
-const visor = document.getElementById('visor');
+        const visor = document.getElementById('visor');
+        const operacaoBotoes = document.querySelectorAll('.operacao');
 
-botoes.forEach(function(botao) {
-    botao.addEventListener('click', function() {
-        
+        let ultimaOperacao = false;
+        let primeiroNumeroDigitado = false;
 
-        const valorBotao = botao.textContent;
+        botoes.forEach(function(botao) {
+            botao.addEventListener('click', function() {
+                const valorBotao = botao.textContent;
 
-        if (valorBotao === '=') {
-            // Executa a expressão matemática
-            const expressao = visor.value;
-            try {
-                const resultado = eval(expressao);
-                visor.value = resultado;
-            } catch (error) {
-                visor.value = 'Erro';
-            }
-        } else if (valorBotao === 'Limpar') {
-            // Limpa o visor
-            visor.value = '';
-        } else {
-            visor.value += valorBotao;
-        }
-
-    });
-});
+                if (valorBotao === '=') {
+                    // Verifica se o último caractere é uma operação e remove, se necessário
+                    const ultimoCaractere = visor.value.charAt(visor.value.length - 1);
+                    if (['+', '-', '*', '/'].includes(ultimoCaractere)) {
+                        visor.value = visor.value.slice(0, -1);
+                    }
+                
+                    // Executa a expressão matemática
+                    const expressao = visor.value;
+                    if (expressao) {
+                        try {
+                            const resultado = eval(expressao);
+                            if (isNaN(resultado)) {
+                                visor.value = 'Erro';
+                            } else {
+                                visor.value = resultado;
+                                primeiroNumeroDigitado = true; // Adiciona esta linha
+                            }
+                        } catch (error) {
+                            visor.value = 'Erro';
+                        }
+                    }
+                
+                    // Reinicia a calculadora após um cálculo bem-sucedido
+                    operacaoBotoes.forEach(function(operacaoBotao) {
+                        operacaoBotao.disabled = false;
+                    });
+                } else if (valorBotao === 'Limpar') {
+                    // Limpa o visor
+                    visor.value = '';
+                    ultimaOperacao = false;
+                    primeiroNumeroDigitado = false;
+                    operacaoBotoes.forEach(function(operacaoBotao) {
+                        operacaoBotao.disabled = true;
+                    });
+                } else if (['*', '/'].includes(valorBotao)) {
+                    if (!primeiroNumeroDigitado) {
+                        // Impede que a operação * ou / seja clicada antes de qualquer número
+                        return;
+                    }
+                    // Verifica se o último caractere é uma operação
+                    const ultimoCaractere = visor.value.charAt(visor.value.length - 1);
+                    if (!ultimaOperacao && ultimoCaractere !== valorBotao) {
+                        visor.value += valorBotao;
+                        ultimaOperacao = true;
+                    } else if (ultimaOperacao && ultimoCaractere !== valorBotao) {
+                        visor.value = visor.value.slice(0, -1) + valorBotao;
+                    }
+                } else {
+                    // Verifica se um cálculo foi realizado antes de reiniciar o visor
+                    if (visor.value === 'Erro') {
+                        visor.value = '';
+                    }
+                    visor.value += valorBotao;
+                    ultimaOperacao = false;
+                
+                    // Habilita as operações quando um número é digitado
+                    operacaoBotoes.forEach(function(operacaoBotao) {
+                        operacaoBotao.disabled = false;
+                    });
+                }
+            });
+        });
